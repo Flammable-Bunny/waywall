d8087c1d725292dc10643c59b53bc9e6511b4a6c
Flammable Bunny <156060069+flammablebunny@users.noreply.github.com>
2025-12-29 15:36:13 -0800

    access token no longer works, dont even try it lol

--- configs/priv.lua
+++ configs/priv.lua
@@ -3,6 +3,6 @@ local PRIV = {}
 PRIV.IRC_IP = "irc.chat.twitch.tv"
 PRIV.IRC_PORT = 6667
 PRIV.IRC_USERNAME = "Flammable_Bunny"
-PRIV.IRC_TOKEN = "oauth:zgjld4eo8ktp3rh0cfr8bxcmasvdda"
+PRIV.IRC_TOKEN = "oauth:XXXXXXXXXXXXXXX"
 
 return PRIV
